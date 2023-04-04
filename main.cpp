fa1c73d5f4eaee467f4705583f73f157946d7f70
pawiercinski@gmail.com <pawiercinski@gmail.com>
2023-04-04 13:04:14 +0200

    Character class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,68 @@
 #include <raylib.h>
 #include <raymath.h>
 
+class Character
+{
+    public:
+        Vector2 getWorldPos() { return worldPos; }
+        Texture2D getTexture() { return texture; }
+        void setScreenPos(int windowWidth, int windowHeight);
+        void tick(float deltaTime);
+    private:
+        Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+        Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+        Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+        Vector2 screenPos{};
+        Vector2 worldPos{};
+        float rightLeft{1.f};
+        float runningTime{};
+        const float updateTime{1.f / 12.f};
+        int frame{};
+        const int maxFrames{6};
+        const float speed{4.f};
+        const float knightScale{4.f};
+};
+
+    void Character::setScreenPos(int windowWidth, int windowHeight)
+    {
+        screenPos = {
+            (float)windowWidth / 2.f - 0.5f * knightScale * texture.width / 6.f,
+            (float)windowHeight / 2.f - 0.5f * knightScale * texture.height
+        };
+    };
+
+    void Character::tick(float deltaTime)
+    {
+        Vector2 direction{};
+
+        if (IsKeyDown(KEY_A)) direction.x -= 1;
+        if (IsKeyDown(KEY_D)) direction.x += 1;
+        if (IsKeyDown(KEY_W)) direction.y -= 1;
+        if (IsKeyDown(KEY_S)) direction.y += 1;
+
+        bool isMoving{Vector2Length(direction) != 0.0};
+
+        if (isMoving)
+        {
+            worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+            direction.x < 0.f ? rightLeft = -1 : rightLeft = 1;
+            texture = run;
+        }
+        else
+            texture = idle;
+
+        runningTime += deltaTime;
+        if (runningTime >= updateTime)
+        {
+            frame > maxFrames ? frame = 0 : frame++;
+            runningTime = 0;
+        }
+
+        Rectangle knightSource{(float)texture.width * frame / 6.f, 0.f, rightLeft * texture.width / 6.f, float(texture.height)};
+        Rectangle knightDest{screenPos.x, screenPos.y, knightScale * texture.width / 6.f, knightScale * texture.height};
+        DrawTexturePro(texture, knightSource, knightDest, Vector2{}, 0.0, WHITE);
+    };
+
 int main()
 {
     const int dimensions[2]{768, 768};
@@ -8,71 +70,26 @@ int main()
 
     Texture2D map = LoadTexture("nature_tileset/map.png");
     Vector2 mapPos{0.0, 0.0};
-    float speed{6.0};
 
-    Texture2D knightIdle = LoadTexture("characters/knight_idle_spritesheet.png");
-    Texture2D knightRunning = LoadTexture("characters/knight_run_spritesheet.png");
-    Texture2D knight{};
-    float knightScale{4.};
-    float KnightWidth{(float)knightIdle.width / 6.f};
-    float KnightHeight{(float)knightIdle.height};
+    Character knight;
 
-    float rightLeft{1.f};
-
-    float runningTime{};
-    const float updateTime{1.f / 12.f};
-    int frame;
-    const int maxFrames{6};
-
-    Vector2 knightPos{
-        dimensions[0] / 2.f - 0.5f * knightScale * KnightWidth,
-        dimensions[1] / 2.f - 0.5f * knightScale * KnightHeight};
+    knight.setScreenPos(dimensions[0], dimensions[1]);
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
-        float dT{GetFrameTime()};
-
         BeginDrawing();
         ClearBackground(WHITE);
-        Vector2 direction{};
 
-        if (IsKeyDown(KEY_A))
-            direction.x -= 1;
-        if (IsKeyDown(KEY_D))
-            direction.x += 1;
-        if (IsKeyDown(KEY_W))
-            direction.y -= 1;
-        if (IsKeyDown(KEY_S))
-            direction.y += 1;
-
-        bool isMoving{Vector2Length(direction) != 0.0};
-
-        if (isMoving)
-        {
-            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
-            direction.x < 0.f ? rightLeft = -1 : rightLeft = 1;
-            knight = knightRunning;
-        }
-        else
-            knight = knightIdle;
+        mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
-        runningTime += dT;
-        if (runningTime >= updateTime)
-        {
-            frame > maxFrames ? frame = 0 : frame++;
-            runningTime = 0;
-        }
-
-        Rectangle knightSource{KnightWidth * frame, 0.f, rightLeft * KnightWidth, KnightHeight};
-        Rectangle knightDest{knightPos.x, knightPos.y, knightScale * KnightWidth, knightScale * KnightHeight};
-        DrawTexturePro(knight, knightSource, knightDest, Vector2{}, 0.0, WHITE);
+        knight.tick(GetFrameTime());
 
         EndDrawing();
     }
     UnloadTexture(map);
-    UnloadTexture(knight);
+    UnloadTexture(knight.getTexture());
 }
